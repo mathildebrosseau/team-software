6cc9e30f08f2618109076385e62275c73c6769c3
Dominic Hains <contact.dominichains@gmail.com>
2024-03-06 13:37:44 -0500

    feat: template

---
+++ packages/client/src/codegen-init.ts
@@ -0,0 +1,15 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  errorsOnly: true,
+  schema: '../../server/schema.gql',
+  documents: './graphql/**/*',
+  generates: {
+    './generated/': {
+      preset: 'client',
+    },
+  },
+};
+
+export default config;
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Provider, createClient } from 'urql';
+
+const client = createClient({
+  url: 'http://localhost:3000/graphql',
+  exchanges: [],
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider value={client}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
